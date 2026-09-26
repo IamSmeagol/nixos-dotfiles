123dace55d7d6d6275ef105ff838359fc9ea208e
Smeagol <thesmeagol360@gmail.com>
2026-09-26 11:29:50 -0400

    update flake.lock

--- flake.nix
+++ flake.nix
@@ -7,7 +7,10 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
